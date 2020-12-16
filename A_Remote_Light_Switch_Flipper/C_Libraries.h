a00910a5fa1efb426088e487c0e3567e394e5461
axz5199 <axz5199@rit.edu>
2020-12-16 01:54:01 -0500

    Divided into multiple files

---
+++ A_Remote_Light_Switch_Flipper/C_Libraries.h
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+#include <Servo.h>
+
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
--- a_Remote_Light_Switch_Flipper/a_Remote_Light_Switch_Flipper.ino
+++ a_Remote_Light_Switch_Flipper/a_Remote_Light_Switch_Flipper.ino
@@ -1,14 +1,5 @@
-/*
-Black button = button 1 (up)
-Green button = button 2 (down)
-*/
-#include <Arduino.h>
-#include <IRremoteESP8266.h>
-#include <IRrecv.h>
-#include <IRutils.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <Servo.h>
+#include "C_Libraries.h"
+#include "B_HTML.h"
 
 #define wifi_status_led 0
 #define orientation_switch 4
@@ -19,70 +10,6 @@ Green button = button 2 (down)
 
 char* ssid = "NETGEAR37";
 char* password = "364h-rbyu-lhtb";
-
-char index_html[] PROGMEM = {"<html>\n"
-"  <head>\n"
-"\t<style>\n"
-"\t\tbody {\n"
-"\t\t  background-color: #212226;\n"
-"\t\t}\n"
-"\n"
-"\t\t.buttons {\n"
-"\t\t  text-align: center;\n"
-"\t\t}\n"
-"\n"
-"\t\t.button1 {\n"
-"\t\t  color: black;\n"
-"\t\t  background-color: #23c48e;\n"
-"\t\t  font-size: 50px;\n"
-"\t\t  width: 180px;\n"
-"\t\t  border: none;\n"
-"\t\t  border-radius: 20px;\n"
-"\t\t  padding: 15px 30px;\n"
-"\t\t  transform: translate(0px, 200px);\n"
-"\t\t  cursor: pointer;\n"
-"\t\t}\n"
-"\n"
-"\t\t.button2 {\n"
-"\t\t  color: black;\n"
-"\t\t  background-color: #23c48e;\n"
-"\t\t  font-size: 50px;\n"
-"\t\t  width: 180px;\n"
-"\t\t  border: none;\n"
-"\t\t  border-radius: 20px;\n"
-"\t\t  padding: 15px 30px;\n"
-"\t\t  transform: translate(0px, 200px);\n"
-"\t\t  cursor: pointer;\n"
-"\t\t}\n"
-"\n"
-"\t\t.button1:hover {\n"
-"\t\t  background-color: #1fad7e\n"
-"\t\t}\n"
-"\n"
-"\t\t.button1:active {\n"
-"\t\t  transform: translateY(190px);\n"
-"\t\t}\n"
-"\n"
-"\t\t.button2:hover {\n"
-"\t\t  background-color: #1fad7e\n"
-"\t\t}\n"
-"\n"
-"\t\t.button2:active {\n"
-"\t\t  transform: translateY(210px);\n"
-"\t\t}\n"
-"\t</style>\n"
-"\t<title>Remote Light Switch</title>\n"
-"\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
-"  </head>\n"
-"  <body class=\"buttons\">\n"
-"   <form action=\"/up\">\n"
-"    <button class=\"button1\"> &#8679 </button>\n"
-"   </form>\n"
-"   <form action=\"/down\">\n"
-"    <button class=\"button2\"> &#8681 </button>\n"
-"   </form>\n"
-"  </body>\n"
-"</html>"};
 
 #define ir_up 0xFF629D
 #define ir_down 0xFFA857
