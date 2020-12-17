9d8ffc498b54384fb6c7f40921561fd57e3a98c2
axz5199 <axz5199@rit.edu>
2020-12-16 20:17:14 -0500

    Added comments

--- A_Remote_Light_Switch_Flipper/D_WIFI_Functions.ino
+++ A_Remote_Light_Switch_Flipper/D_WIFI_Functions.ino
@@ -1,28 +1,28 @@
-void handleRoot()
+void handleRoot()                                //When "ip_address" is entered in the address bar
 {
-  server.send_P(200, "text/html", index_html);
+  server.send_P(200, "text/html", index_html);   //Send response code and html to webpage
 }
 
-void handleUp()
+void handleUp()                                  //When "ip_address/up" is entered in the address bar
 {
   Serial.println("Wifi up");
-  myservo.attach(servo_pin);
-  myservo.write(servo_position_up);
-  server.send_P(200, "text/html", index_html);
-  delay(500);
+  myservo.attach(servo_pin);                     //Attach servo motor
+  myservo.write(servo_position_up);              //Move arm up
+  server.send_P(200, "text/html", index_html);   //Send response code and html to webpage
+  delay(500);                                    //Provides sufficient time for arm to move
   myservo.write(servo_position_mid);
-  delay(500);
-  myservo.detach();
+  delay(500);                                    //Provides sufficient time for arm to move
+  myservo.detach();                              //Detach servo motor to conserve power
 }
 
-void handleDown()
+void handleDown()                                //When "ip_address/down" is entered in the address bar
 {
   Serial.println("Wifi down");
-  myservo.attach(servo_pin);
-  myservo.write(servo_position_down);
-  server.send_P(200, "text/html", index_html);
-  delay(500);
+  myservo.attach(servo_pin);                     //Attach servo motor
+  myservo.write(servo_position_down);            //Move arm down
+  server.send_P(200, "text/html", index_html);   //Send response code and html to webpage
+  delay(500);                                    //Provides sufficient time for arm to move
   myservo.write(servo_position_mid);
-  delay(500);
-  myservo.detach();
+  delay(500);                                    //Provides sufficient time for arm to move
+  myservo.detach();                              //Detach servo motor to conserve power
 }
--- a_Remote_Light_Switch_Flipper/a_Remote_Light_Switch_Flipper.ino
+++ a_Remote_Light_Switch_Flipper/a_Remote_Light_Switch_Flipper.ino
@@ -11,14 +11,14 @@
 char* ssid = "NETGEAR37";
 char* password = "364h-rbyu-lhtb";
 
-#define ir_up 0xFF629D
-#define ir_down 0xFFA857
+#define ir_up 0xFF629D          //IR code for up button on Keyes IR remote
+#define ir_down 0xFFA857        //IR code for down button on Keyes IR remote
 
 int wifi_connection_timer = 0; //Measures the amount of time spent trying to connect to wifi
 
-int servo_position_up = 130;
-int servo_position_mid = 90;
-int servo_position_down = 50;
+int servo_position_up = 130;  //130 deg is up when servo motor is attached on the right side
+int servo_position_mid = 90;  //90 deg is default middle position
+int servo_position_down = 50; //50 deg is down when servo motor is attached on the left side
 
 ESP8266WebServer server;
 Servo myservo;
@@ -27,128 +27,148 @@ decode_results results;
 
 void setup()
 {
-  //Pinmodes
-  pinMode(wifi_status_led, OUTPUT); // Built-in led pin
-  pinMode(servo_pin, OUTPUT); // Servo control pin
-  pinMode(button1, INPUT_PULLUP); //Button input pin
-  pinMode(button2, INPUT_PULLUP); //Button input pin
+  //PINMODES//
+  pinMode(wifi_status_led, OUTPUT);          // Built-in led pin
+  pinMode(servo_pin, OUTPUT);                // Servo control pin
+  pinMode(button1, INPUT_PULLUP);            //Button input pin
+  pinMode(button2, INPUT_PULLUP);            //Button input pin
   pinMode(orientation_switch, INPUT_PULLUP); //Sliding switch input pin
+  //PINMODES//
 
-  //Esablish orientation
-  if (digitalRead(orientation_switch) == 0) //Servo on left side
+
+  //ESTABLISH ORIENTATION//
+  if (digitalRead(orientation_switch) == 0) //When servo on left side
   {
     servo_position_up = 50;
     servo_position_down = 130;
   }
-  else //Servo on right side
+  else                                      //When servo on right side
   {
     servo_position_up = 130;
     servo_position_down = 50;
   }
-  
+  //ESTABLISH ORIENTATION//
 
-  //Setup
+  
+  //ENABLE HARDWARE AND WIFI//
   Serial.begin(115200); delay(15); Serial.println("");
-  myservo.attach(servo_pin); //Servo setup
-  myservo.write(servo_position_mid);
-  irrecv.enableIRIn(); //Start the receiver
-  WiFi.begin(ssid, password); //Wifi setup
+  myservo.attach(servo_pin);              //Attach servo
+  myservo.write(servo_position_mid);      //Set servo arm to default position
+  irrecv.enableIRIn();                    //Start the receiver
+  WiFi.begin(ssid, password);             //Enter wifi credentials
+  //ENABLE HARDWARE AND WIFI//
+
   
-  //Connect to wifi
-  while(WiFi.status()!=WL_CONNECTED)
+  //CONNECT TO WIFI//
+  while(WiFi.status()!=WL_CONNECTED)        //Loop while wifi is attempting to connect
   {
-    Serial.print(".");
-    digitalWrite(wifi_status_led, LOW);
+    Serial.print(".");                      //Indicates loading
+    digitalWrite(wifi_status_led, LOW);     //Turn on led attached to pin 0
     delay(500);
-    Serial.print(".");
-    digitalWrite(wifi_status_led, HIGH);
+    Serial.print(".");                      //Indicates loading
+    digitalWrite(wifi_status_led, HIGH);    //Turn off led attached to pin 0
     delay(500);
-    wifi_connection_timer++;
-    if (wifi_connection_timer == 10) break;
+    wifi_connection_timer++;                //Increments counter every 1 second
+    if (wifi_connection_timer == 10) break; //Stop attempting to connect to wifi if unsuccessful after 10 seconds
   }
+  //CONNECT TO WIFI//
 
-  Serial.println("");
 
-  //Establish HTML routes if wifi successfully connects
-  if (WiFi.status()==WL_CONNECTED)
+  Serial.println(""); //Skip line after loading dots
+  
+
+  //ESTABLISH HTML ROUTES//
+  if (WiFi.status()==WL_CONNECTED)        //Run only when wifi is connected
   {
-    server.on("/", handleRoot); //Call function to handle root
-    server.on("/up", handleUp); //Call function to handle up command
-    server.on("/down", handleDown); //Call function to handle down command
-    server.begin(); //Start server
-    Serial.print("Connected to "); Serial.println(ssid);
-    Serial.print("IP Address: "); Serial.println(WiFi.localIP());
-    digitalWrite(wifi_status_led, LOW);
+    server.on("/", handleRoot);           //Call function to handle root
+    server.on("/up", handleUp);           //Call function to handle up command
+    server.on("/down", handleDown);       //Call function to handle down command
+    server.begin();                       //Start server
+    Serial.print("Connected to ");  
+    Serial.println(ssid);                 //Print network name
+    Serial.print("IP Address: ");   
+    Serial.println(WiFi.localIP());       //Print network IP address
+    digitalWrite(wifi_status_led, LOW);   //Wifi indicator led steady on when connected
   }
-  else //Skip wifi setup if it fails to connect
+  else                                    //Skip wifi setup if it fails to connect
   {
     Serial.println("Wifi failed to connect");
-    digitalWrite(wifi_status_led, HIGH);
+    digitalWrite(wifi_status_led, HIGH);  //Wifi indicator led off when no wifi connection
   }
-  myservo.detach();
+  //ESTABLISH HTML ROUTES//
+
+  myservo.detach(); //Turn off servo to conserve power
 }
+//END setup()
 
 void loop()
 {
-  delay(1);
+  delay(1); //Allows ESP8266 to modem sleep
   
-  //Wifi control
+  //WIFI CONTROL//
   server.handleClient();
+  //WIFI CONTROL//
 
-  //Physical button control
-  if (digitalRead(button1) == 0)
+
+  //PUSH BUTTON CONTROL//
+  if (digitalRead(button1) == 0)  //When the up button is pressed
   {
     Serial.println("PB up");
-    myservo.attach(servo_pin);
-    myservo.write(130);
-    delay(500);
-    myservo.write(90);
-    delay(500);
-    myservo.detach();
+    myservo.attach(servo_pin);    //Attach servo motor
+    myservo.write(130);           //Move arm up
+    delay(500);                   //Provides sufficient time for arm to move
+    myservo.write(90);            //Move arm back to mid
+    delay(500);                   //Provides sufficient time for arm to move
+    myservo.detach();             //Detach servo motor to conserve power
   }
 
-  if (digitalRead(button2) == 0)
+  if (digitalRead(button2) == 0)  //When the down button is pressed
   {
     Serial.println("PB down");
-    myservo.attach(servo_pin);
-    myservo.write(50);
-    delay(500);
-    myservo.write(90);
-    delay(500);
-    myservo.detach();
+    myservo.attach(servo_pin);    //Attach servo motor
+    myservo.write(50);            //Move arm down
+    delay(500);                   //Provides sufficient time for arm to move
+    myservo.write(90);            //Move arm back to mid
+    delay(500);                   //Provides sufficient time for arm to move
+    myservo.detach();             //Detach servo motor to conserve power
   }
+  //PUSH BUTTON CONTROL//
+  
 
-  //IR control
-  if (irrecv.decode(&results)) {
-
+  //IR CONTROL//
+  if (irrecv.decode(&results))              //If ir receiver obtains a signal
+  {
     serialPrintUint64(results.value, HEX);
     Serial.print(" ");
 
-    switch (results.value)
+    switch (results.value)                  //Check ir code
     {
-      case ir_up:
+      case ir_up:                           //When up code (0xFF629D)
         Serial.println("IR up");
-        myservo.attach(servo_pin);
-        myservo.write(servo_position_up);
-        delay(500);
-        myservo.write(servo_position_mid);
-        delay(500);
-        myservo.detach();
+        myservo.attach(servo_pin);          //Attach servo motor
+        myservo.write(servo_position_up);   //Move arm up
+        delay(500);                         //Provides sufficient time for arm to move
+        myservo.write(servo_position_mid);  //Move arm back to mid
+        delay(500);                         //Provides sufficient time for arm to move
+        myservo.detach();                   //Detach servo motor to conserve power
         break;
 
-      case ir_down:
+      case ir_down:                         //When down code (0xFFA857)
         Serial.println("IR down");
-        myservo.attach(servo_pin);
-        myservo.write(servo_position_down);
-        delay(500);
-        myservo.write(servo_position_mid);
-        delay(500);
-        myservo.detach();
+        myservo.attach(servo_pin);          //Attach servo motor
+        myservo.write(servo_position_down); //Move arm down
+        delay(500);                         //Provides sufficient time for arm to move
+        myservo.write(servo_position_mid);  //Move arm back to mid
+        delay(500);                         //Provides sufficient time for arm to move
+        myservo.detach();                   //Detach servo motor to conserve power
         break;
 
-      default:
+      default:                              //When code is undefined
         Serial.println("IR misc");
     }
-    irrecv.resume();  //Receive the next value
+    
+    irrecv.resume();                        //Receive the next code
   }
+  //IR CONTROL//
 }
+//END loop()
